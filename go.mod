27edd94bea5a368250e70cda28b62dd4990a4bc7
nntaoli <2767415655@qq.com>
2022-10-10 21:37:51 +0800

    urfave cli处理命令参数；添加批量rename功能

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nntaoli/find-replace
 
-go 1.12
+go 1.16
+
+require github.com/urfave/cli/v2 v2.19.2
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"flag"
+	"github.com/urfave/cli/v2"
 	"io/ioutil"
 	"log"
+	"os"
+	"path"
+	"path/filepath"
 	"regexp"
+	"strings"
 )
 
 var (
@@ -30,32 +35,122 @@ func allFiles(dir string) {
 	}
 }
 
-func findAndReplaceAll(files []string) {
-	oldReg, _ := regexp.Compile(*old)
+func findAndReplaceAll(old, new string, replace bool) {
+	oldReg, _ := regexp.Compile(old)
 	for _, f := range files {
 		data, err := ioutil.ReadFile(f)
 		if err != nil {
 			log.Println("[err] read  ", f, " err => ", err)
 			continue
 		}
-
-		if *replaceAllCmd {
+		dataStr := string(data)
+		if replace {
 			log.Println("[info] replace ", f)
-			newData := oldReg.ReplaceAllString(string(data), *new)
+			newData := oldReg.ReplaceAllString(dataStr, new)
 			err = ioutil.WriteFile(f, []byte(newData), 644)
 			if err != nil {
 				log.Println("[err] write ", f, " error ", err)
 			}
 		} else {
-			if oldReg.MatchString(*old) {
+			if oldReg.MatchString(dataStr) {
+				indes := oldReg.FindAllStringIndex(dataStr, len(old))
+				for _, idx := range indes {
+					if f == "/home/dev/data2/workspace/find-replace/find-replace" {
+						log.Println(dataStr[idx[0]-12 : idx[1]+11])
+					}
+
+				}
 				log.Println("[info] find ", f)
 			}
 		}
 	}
 }
 
+func readAllFileName(dir string) {
+	fileInfos, err := ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+	for _, f := range fileInfos {
+		if f.IsDir() {
+			allFiles(dir + "/" + f.Name())
+			continue
+		}
+		files = append(files, path.Join(dir, f.Name()))
+	}
+}
+
+func batchRename(old, new string) {
+	for _, f := range files {
+		dir, name := filepath.Split(f)
+		newName := strings.ReplaceAll(name, old, new)
+		err := os.Rename(f, filepath.Join(dir, newName))
+		if err != nil {
+			log.Println("[error] rename  [", f, "] . err=", err.Error())
+			continue
+		}
+		log.Println("[info] rename success new :", filepath.Join(dir, newName))
+	}
+}
+
 func main() {
-	flag.Parse()
-	allFiles(flag.Arg(len(flag.Args()) - 1))
-	findAndReplaceAll(files)
+	app := &cli.App{
+		Name: "find-replace",
+		Commands: []*cli.Command{
+			{
+				Name:  "content",
+				Usage: "replace content",
+				Flags: []cli.Flag{
+					&cli.BoolFlag{
+						Name:  "replace",
+						Usage: "true: find and replace \n false: only find",
+					},
+					&cli.StringFlag{
+						Name:     "old",
+						Usage:    "old string",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:  "new",
+						Usage: "new string",
+					},
+				},
+				Action: func(context *cli.Context) error {
+					if context.NArg() == 0 {
+						panic("please input search dir path")
+					}
+					allFiles(context.Args().Get(0))
+					findAndReplaceAll(context.String("old"), context.String("new"), context.Bool("replace"))
+					return nil
+				},
+			},
+			{
+				Name:  "rename",
+				Usage: "rename file name",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "old",
+						Usage:    "old string",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:  "new",
+						Usage: "new string",
+					},
+				},
+				Action: func(context *cli.Context) error {
+					if context.NArg() == 0 {
+						panic("please input search dir path")
+					}
+					allFiles(context.Args().Get(0))
+					batchRename(context.String("old"), context.String("new"))
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
